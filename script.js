d32b904c9118e91fe243c2261bda908d11aa584e
Michael Harrsion <harrisonmike73@yahoo.com>
2022-02-10 17:51:21 -0600

    -A

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const finalColumn = document.querySelector('[data-final-column]')
-const computerScoreSpan = document.querySelector('[data-computer-score]')
-const yourScoreSpan = document.querySelector('[data-your-score]')
-const winMsg = document.querySelector('winMsg')
-const SELECTIONS = [
-    {
-        name: 'rock',
-        emoji: '✊',
-        beats: 'scissors'
-    },
-    {
-        name: 'paper',
-        emoji: '✋',
-        beats: 'rock'
-    },
-    {
-        name: 'scissors',
-        emoji: '✌',
-        beats: 'paper'
-    },
-]
 
-selectionButtons.forEach(selectionButton => {
-    selectionButton.addEventListener('click', e => {
-      const selectionName = selectionButton.dataset.selection
-      const selection = SELECTIONS.find(selection => selection.name === selectionName)
-      makeSelection(selection)
-    })
-})
-
-function makeSelection(selection) {
-    const computerSelection = randomSelection()
-    const yourWinner = isWinner(selection, computerSelection)
-    const computerWinner = isWinner(computerSelection, selection)
+    let playerScore = 0
+    let computerScore = 0
     
-    addSelectionResult(computerSelection, computerWinner)
-    addSelectionResult(selection, yourWinner)
+    const choices = ["rock", "paper", "scissors"];
+ 
+            // Asks the user to input rock, paper, or scissors and returns answer to lower case.
+        
+            let playerSelection = prompt("Do you choose Rock, Paper or scissors?").toLowerCase();
+    
+            // generates random number between 0-2. Uses 'const choices' to return rock, paper, or scissors.
+        function computerSelection() {
+           return choices [Math.floor(Math.random()* 3)];
+        }
 
-    if (yourWinner) incrementScore(yourScoreSpan)
-    if (computerWinner) incrementScore(computerScoreSpan)
-}
 
-function incrementScore(scoreSpan) {
-    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
+         function playRound (playerSelection, computerSelection) 
+         {
+             // rock logic
 
-function addSelectionResult(selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = selection.emoji
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner')
-    finalColumn.after(div)
-}
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name
-}
+                if (playerSelection == "rock" && computerSelection == "rock") {
+                    return "It/'s a draw!";
+                } else if (playerSelection == "rock" && computerSelection == "paper") {
+                    return "You lose! Paper covers rock!"
+                    computerScore += 1;
+                } else if (playerSelection == "rock" && computerSelection == "scissors") {
+                    return "You win! Rock smashes scissors!"
+                    playerScore += 1; 
+                }
+            // paper logic
+                if (playerSelection == "paper" && computerSelection == "paper") {
+                    return "It/'s a draw!";
+                } else if (playerSelection == "paper" && computerSelection == "rock") {
+                    return "You win! Paper covers rock!"
+                    playerScore += 1; 
+                } else if (playerSelection == "paper" && computerSelection == "scissors") {
+                    return "You lose! Scissors cuts paper!"
+                    computerScore += 1;
+                }
+            // scissors logic
+                if (playerSelection == "scissors" && computerSelection == "scissors") {
+                    return "It/'s a draw!"; 
+                }  else if (playerSelection == "scissors" && computerSelection == "rock") {
+                    return "You lose! Rock smashes scissors!"
+                    computerScore += 1; 
+                }  else if (playerSelection == "scissors" && computerSelection == "paper") {
+                    return "You win! Scissors cuts paper!"
+                    playerScore += 1;
+                }   
+                    else return "Something went wrong. Please try again.";
+         
+                }
+           
+         if (playerScore = 5) {
+         alert("You win the game!");}
+         else if (computerScore = 5) {
+             alert("You lose.. better luck next time.")
+         }
 
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS[randomIndex]
-}
+            function game() {
+                
+            }
+         
+         console.log(playRound(playerSelection, computerSelection()));
+                
+                
+
+    
