1e7ef840dbd3f2570196fde7dc4e93d4c99e98c2
Michael Harrsion <harrisonmike73@yahoo.com>
2022-03-02 18:46:45 -0600

    -A

--- RPS2/code.js
+++ RPS2/code.js
@@ -2,58 +2,53 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
- 
- 
+
+
+
     const playGame = () => {
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
         const scissorBtn = document.querySelector('.scissor');
         const playerOptions = [rockBtn,paperBtn,scissorBtn];
         const computerOptions = ['rock','paper','scissors']
-         
 
+        // Function to start playing
         playerOptions.forEach(option => {
             option.addEventListener('click',function(){
- 
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerText = `Moves Left: ${5-moves}`;
-                 
- 
+
+
                 const choiceNumber = Math.floor(Math.random()*3);
                 const computerChoice = computerOptions[choiceNumber];
- 
-
+                
                 winner(this.innerText,computerChoice)
-                 
-
+                
                 if(moves == 5){
                     gameOver(playerOptions,movesLeft);
                 }
             })
         })
-         
-    }
- 
 
+    }
+
+    // Function to decide winner
     const winner = (player,computer) => {
         const result = document.querySelector('.result');
         const playerScoreBoard = document.querySelector('.p-count');
         const computerScoreBoard = document.querySelector('.c-count');
-        //const computerChose = document.querySelector('.computerChose');
         player = player.toLowerCase();
         computer = computer.toLowerCase();
         if(player === computer){
             result.textContent = 'Tie'
         }
-        
         else if(player == 'rock'){
             if(computer == 'paper'){
-                //computerChose.textContent = 'Computer chose Paper';
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
- 
+
             }else{
                 result.textContent = 'Player Won'
                 playerScore++;
@@ -62,7 +57,6 @@ const game = () => {
         }
         else if(player == 'scissors'){
             if(computer == 'rock'){
-                //computerChose.textContent = 'Computer chose rock';
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
@@ -74,7 +68,6 @@ const game = () => {
         }
         else if(player == 'paper'){
             if(computer == 'scissors'){
-                //computerChose.textContent = 'Computer chose scissors';
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
@@ -82,41 +75,37 @@ const game = () => {
                 result.textContent = 'Player Won';
                 playerScore++;
                 playerScoreBoard.textContent = playerScore;
-
-                result.textContent = computer;
             }
         }
     }
- 
-    
 
     const gameOver = (playerOptions,movesLeft) => {
- 
+
         const chooseMove = document.querySelector('.move');
         const result = document.querySelector('.result');
         const reloadBtn = document.querySelector('.reload');
- 
+
         playerOptions.forEach(option => {
             option.style.display = 'none';
         })
- 
-      
-        chooseMove.innerText = 'Game Over!!'
+
+
+        chooseMove.innerText = 'Game Over'
         movesLeft.style.display = 'none';
- 
+
         if(playerScore > computerScore){
             result.style.fontSize = '2rem';
-            result.innerText = 'You Won The Game'
+            result.innerText = 'Player Won'
             result.style.color = '#308D46';
         }
         else if(playerScore < computerScore){
             result.style.fontSize = '2rem';
-            result.innerText = 'You Lost The Game';
+            result.innerText = 'Computer Won';
             result.style.color = 'red';
         }
         else{
             result.style.fontSize = '2rem';
-            result.innerText = 'Tie';
+            result.innerText = 'Tie Game';
             result.style.color = 'grey'
         }
         reloadBtn.innerText = 'Restart';
@@ -125,9 +114,9 @@ const game = () => {
             window.location.reload();
         })
     }
- 
+
     playGame();
-     
+
 }
 
 game();
